4917dd77209f78eec46950bf49421db8c3d6436b
Cody Carman <codyjcarman@gmail.com>
2024-06-10 19:02:50 -0400

    moved css to separate file

--- accordion.js
+++ accordion.js
@@ -1,12 +1,14 @@
-! function() {
-    for (var l = document.querySelectorAll(".accordion .menu"), e = 0; e < l.length; e++) l[e].addEventListener("click", n);
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-    function n() {
-        for (var e = document.querySelectorAll(".accordion .panel"), n = 0; n < e.length; n++) e[n].className = "panel close";
-        if (-1 == this.className.indexOf("active")) {
-            for (n = 0; n < l.length; n++) l[n].className = "menu";
-            this.className = "menu active", this.nextElementSibling.className = "panel open"
-        } else
-            for (n = 0; n < l.length; n++) l[n].className = "menu"
-    }
-}();
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function() {
+	this.classList.toggle("active");
+	var panel = this.nextElementSibling;
+	if (panel.style.maxHeight) {
+		panel.style.maxHeight = null;
+	} else {
+		panel.style.maxHeight = panel.scrollHeight + "px";
+	} 
+	});
+}
