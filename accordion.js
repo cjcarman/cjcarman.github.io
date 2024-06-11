5f11c84b93ee0a26ace20b9d824f8d43cc2cf3c7
Cody Carman <codyjcarman@gmail.com>
2024-06-10 20:06:38 -0400

    more changes

--- accordion.js
+++ accordion.js
@@ -9,17 +9,19 @@
     {
         for (var e = document.querySelectorAll(".my-accordion .panel"), j = 0; j < e.length; j++)
         {
-            e[n].className = "panel close"
+            e[j].className = "panel close"
         }
 
         if (-1 == this.className.indexOf("active"))
         {
-            for (k = 0; k < accordion.length; k++) accordion[k].className = "menu";
+            for (k = 0; k < accordion.length; k++)
+                accordion[k].className = "menu";
 
             this.className = "menu active", this.nextElementSibling.className = "panel open"
         } else
         {
-            for (l = 0; l < accordion.length; l++) accordion[l].className = "menu"
+            for (l = 0; l < accordion.length; l++)
+                accordion[l].className = "menu"
         }
     }
 }();
